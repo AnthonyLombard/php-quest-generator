8ea748cad238102302fc6678c01275103958f31e
Anthony Lombard <anthony.isalive@gmail.com>
2018-04-03 13:39:41 +0200

    initial frontend testing

--- quest-generator.php
+++ quest-generator.php
@@ -4,11 +4,16 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Quest generator in php</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    
+<div class="container">
+<div class="quest-panel">
+    <div class="title"><h1>QUEST GENERATOR</h1></div>
+        <div class="quest"></div>
+    </div>
+</div>
+
 </body>
 </html>
