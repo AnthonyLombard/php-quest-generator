5dae30e6119cda10366e0b7d96c463f605075ada
Anthony Lombard <anthony.isalive@gmail.com>
2018-04-03 13:53:18 +0200

    CSS fix and added quest info divs

--- quest-generator.php
+++ quest-generator.php
@@ -11,7 +11,16 @@
 <div class="container">
 <div class="quest-panel">
     <div class="title"><h1>QUEST GENERATOR</h1></div>
-        <div class="quest"></div>
+        <div class="quest">
+            <div class="quest-title"></div>
+            <div class="quest-location"></div>
+            <div class="quest-details"></div>
+            <div class="quest-a-d">
+                <div class="quest-accept"></div>
+                <div class="quest-decline"></div>
+            </div>
+
+        </div>
     </div>
 </div>
 
